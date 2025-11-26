26a299b85a46fde5961532b228c975fb16fe9172
0DragonSoul0 <stepina.erika@mail.ru>
2025-11-26 12:10:44 +0200

    Add files via upload

---
+++ Lab_9_Stepina_Erika_variant_10/Lab_9_Stepina_Erika_variant_10/Lab_9_Stepina_Erika_variant_10.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <locale.h>
+
+int main() {
+	//1 способ
+
+	setlocale(LC_ALL, "Russian");
+	long long number;
+	std::cout << "Введите желаемое число типа long long:" << std::endl;
+	std::cin >> number;
+	long long* pointer = &number;
+	std::cout << "Содержимое байтов числа:" << std::endl;
+	for (int i = 0; i < sizeof(number); ++i) {
+		unsigned char byte = *(((unsigned char*)pointer) + i);
+		int c = byte;
+		std::cout << c << " ";
+	}
+	return 0;
+
+	//2 способ.
+
+	/*long long number;
+	std::cout << "Введите желаемое число типа long long:" << std::endl;
+	std::cin >> number;
+	unsigned char* pointer = (unsigned char*) &number;
+	for (int i = 0; i < sizeof(number); ++i) {
+		int byte = *(pointer + i);
+		std::cout << byte << " ";
+	}
+	return 0;*/
+}
